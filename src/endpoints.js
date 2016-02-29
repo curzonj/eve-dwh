307534ab04c5d7f6117133573808b616817d4961
Jordan Curzon <curzonj@gmail.com>
2016-02-28 17:52:58 -0800

    add a redirect

--- src/endpoints.js
+++ src/endpoints.js
@@ -3,4 +3,8 @@
 module.exports = function(app) {
     app.use('/api', require('./endpoints/api'))
     app.use('/_', require('./endpoints/debug'))
+
+    app.get('/', function(req, res) {
+        res.redirect("/test.html")
+    })
 }

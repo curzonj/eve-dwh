41aeeade567229b20536fac3ef31a3f9a1edba25
Jordan Curzon <curzonj@gmail.com>
2016-03-28 14:36:33 -0700

    scaled axis with zoom preview window

--- public/app.js
+++ public/app.js
@@ -49,6 +49,7 @@
 	global.orderNotificationsPage = __webpack_require__(1)
 	global.rickshawPage = function() {
 	  const Rickshaw = global.Rickshaw
+	  const d3 = global.d3
 	  const _ = __webpack_require__(50)
 	  const axios = __webpack_require__(73)
 	  const bluebird = __webpack_require__(91)
@@ -65,6 +66,14 @@
 	      columns: 'buy_price_max,sell_price_min',
 	    },
 	  }).then(response => {
+	    const min = _.reduce(response.data, (result, row) => {
+	      return Math.min(result, row.buy_price_max)
+	    }, Number.MAX_VALUE)
+	    const max = _.reduce(response.data, (result, row) => {
+	      return Math.max(result, row.sell_price_min)
+	    }, Number.MIN_VALUE)
+	    const scale = d3.scale.linear().domain([min, max]).nice()
+
 	    const palette = new Rickshaw.Color.Palette();
 	    const graph = new Rickshaw.Graph({
 	      element: document.getElementById('chart'),
@@ -72,9 +81,12 @@
 	      height: 600,
 	      renderer: 'line',
 	      offset: 'lines',
+	      stack: false,
+	      interpolation: 'step-after',
 	      series: [{
 	        name: 'buy_price_max',
 	        color: palette.color(),
+	        scale: scale,
 	        data: _.map(response.data, r => {
 	          return {
 	            x: r.unix_ts,
@@ -84,6 +96,7 @@
 	      }, {
 	        name: 'sell_price_min',
 	        color: palette.color(),
+	        scale: scale,
 	        data: _.map(response.data, r => {
 	          return {
 	            x: r.unix_ts,
@@ -95,12 +108,17 @@
 	    const x_axis = new Rickshaw.Graph.Axis.Time({
 	      graph: graph,
 	    })
-	    var y_axis = new Rickshaw.Graph.Axis.Y({
+	    var y_axis = new Rickshaw.Graph.Axis.Y.Scaled({
 	      graph: graph,
 	      orientation: 'left',
+	      scale: scale,
 	      tickFormat: Rickshaw.Fixtures.Number.formatKMBT,
 	      element: document.getElementById('y_axis'),
 	    });
+	    var slider = new Rickshaw.Graph.RangeSlider.Preview({
+	      graph: graph,
+	      element: document.querySelector('#slider'),
+	    })
 	    new Rickshaw.Graph.HoverDetail({
 	      graph: graph,
 	    })
--- src/ui/entry.js
+++ src/ui/entry.js
@@ -3,6 +3,7 @@
 global.orderNotificationsPage = require('./orders')
 global.rickshawPage = function() {
   const Rickshaw = global.Rickshaw
+  const d3 = global.d3
   const _ = require('lodash')
   const axios = require('axios')
   const bluebird = require('bluebird')
@@ -19,6 +20,14 @@ global.rickshawPage = function() {
       columns: 'buy_price_max,sell_price_min',
     },
   }).then(response => {
+    const min = _.reduce(response.data, (result, row) => {
+      return Math.min(result, row.buy_price_max)
+    }, Number.MAX_VALUE)
+    const max = _.reduce(response.data, (result, row) => {
+      return Math.max(result, row.sell_price_min)
+    }, Number.MIN_VALUE)
+    const scale = d3.scale.linear().domain([min, max]).nice()
+
     const palette = new Rickshaw.Color.Palette();
     const graph = new Rickshaw.Graph({
       element: document.getElementById('chart'),
@@ -26,9 +35,12 @@ global.rickshawPage = function() {
       height: 600,
       renderer: 'line',
       offset: 'lines',
+      stack: false,
+      interpolation: 'step-after',
       series: [{
         name: 'buy_price_max',
         color: palette.color(),
+        scale: scale,
         data: _.map(response.data, r => {
           return {
             x: r.unix_ts,
@@ -38,6 +50,7 @@ global.rickshawPage = function() {
       }, {
         name: 'sell_price_min',
         color: palette.color(),
+        scale: scale,
         data: _.map(response.data, r => {
           return {
             x: r.unix_ts,
@@ -49,12 +62,17 @@ global.rickshawPage = function() {
     const x_axis = new Rickshaw.Graph.Axis.Time({
       graph: graph,
     })
-    var y_axis = new Rickshaw.Graph.Axis.Y({
+    var y_axis = new Rickshaw.Graph.Axis.Y.Scaled({
       graph: graph,
       orientation: 'left',
+      scale: scale,
       tickFormat: Rickshaw.Fixtures.Number.formatKMBT,
       element: document.getElementById('y_axis'),
     });
+    var slider = new Rickshaw.Graph.RangeSlider.Preview({
+      graph: graph,
+      element: document.querySelector('#slider'),
+    })
     new Rickshaw.Graph.HoverDetail({
       graph: graph,
     })

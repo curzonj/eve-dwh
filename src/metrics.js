e9176d010028db68723845672b20bc9dbcb34ee6
Jordan Curzon <curzonj@gmail.com>
2016-04-01 15:54:19 -0700

    format src/metrics.js better

--- src/metrics.js
+++ src/metrics.js
@@ -10,36 +10,42 @@ const proc_type = process.env.PROC_TYPE
 
 
 bluebird.try(function() {
-    debug("metrics", "Initialization started")
-    return sql("metric_observations").where({ hostname: hostname, proc_type: proc_type }).delete()
+  debug('metrics', 'Initialization started')
+  return sql('metric_observations').where({
+    hostname: hostname,
+    proc_type: proc_type,
+  }).delete()
 }).then(function() {
-    return sql("metric_observations").insert({
-        hostname: hostname,
-        updated_at: sql.raw("current_timestamp"),
-        proc_type: proc_type,
-        metrics: {}
-    })
+  return sql('metric_observations').insert({
+    hostname: hostname,
+    updated_at: sql.raw('current_timestamp'),
+    proc_type: proc_type,
+    metrics: {},
+  })
 }).then(function() {
-    setInterval(function() {
-        const data = stats.toJSON()
-        debug("metrics", JSON.stringify(data, null, 2))
+  setInterval(function() {
+    const data = stats.toJSON()
+    debug('metrics', JSON.stringify(data, null, 2))
 
-        sql("metric_observations").where({ hostname: hostname, proc_type: proc_type }).update({
-            updated_at: sql.raw("current_timestamp"),
-            metrics: data
-        }).then(function() {
-            debug("metrics", "Metrics recorded to db")
-        })
-    }, 60000)
+    sql('metric_observations').where({
+      hostname: hostname,
+      proc_type: proc_type,
+    }).update({
+      updated_at: sql.raw('current_timestamp'),
+      metrics: data,
+    }).then(function() {
+      debug('metrics', 'Metrics recorded to db')
+    })
+  }, 60000)
 }).then(function() {
-    debug('metrics', 'Recording initialized')
+  debug('metrics', 'Recording initialized')
 })
 
 // Cleanup old metrics every hour
 setInterval(function() {
-    sql("metric_observations").whereRaw("updated_at < now() - interval '1 day'").delete()
+  sql('metric_observations').whereRaw('updated_at < now() - interval \'1 day\'').delete()
 }, 360000)
 
 module.exports = {
-    stats: stats
+  stats: stats,
 }

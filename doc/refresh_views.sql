06e9a944f99c5e56c1ebdf3c7446dbba2665aad1
Jordan Curzon <curzonj@gmail.com>
2016-04-09 01:03:33 -0700

    fix docs sql

--- doc/refresh_views.sql
+++ doc/refresh_views.sql
@@ -2,3 +2,4 @@ refresh materialized view order_frequencies;
 refresh materialized view trade_hub_stats;
 refresh materialized view market_group_arrays;
 refresh materialized view type_metas;
+refresh materialized view observed_history;
